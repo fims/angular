ecb0a1efdbf7e9b7ebc5ac63bd5875c89d9724b7
Fabian Imsand <fabian.imsand@hevs.ch>
2016-05-30 17:33:42 +0200

    basic binding

--- app/app.js
+++ app/app.js
@@ -20,14 +20,9 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'main/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
